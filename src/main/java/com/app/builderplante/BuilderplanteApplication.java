64303049eebcd3dd9ea9e48d31248813dae23eaa
Jemisse Macuacua <jemissemacuacua@Mac.localdomain>
2026-05-28 12:26:05 +0200

    feat(project-setup): initialize Spring Boot architecture with Maven base files

---
+++ src/main/java/com/app/builderplante/BuilderplanteApplication.java
@@ -0,0 +1,13 @@
+package com.app.builderplante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuilderplanteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuilderplanteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/builderplante/BuilderplanteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.builderplante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuilderplanteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
